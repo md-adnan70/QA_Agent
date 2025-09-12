441926fe33d65f9806068d5e7b72a8497ef4d9b0
adnan rao <rao.adnan.098@gmail.com>
2025-09-12 17:00:23 +0530

    All Project Files created

---
+++ template.sh
@@ -0,0 +1,13 @@
+#Creating directory
+mkdir -p src
+mkdir -p data
+mkdir -p vector_store
+
+
+#creating file
+touch src/ingest.py
+touch src/agent.py
+touch requirements.txt
+touch .env
+
+echo "Directory and files created successfully"
